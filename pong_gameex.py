c7741294bc0934ceaa2d1ba342ca3ad22177054b
lorguan <107538910+lorguan@users.noreply.github.com>
2022-07-01 03:03:14 -0500

    first commit

---
+++ pong_gameex.py
@@ -0,0 +1,141 @@
+# Simple Pong in Python 3 for beginners
+# extra modifications hehe
+
+
+
+import turtle
+import os
+
+wn = turtle.Screen()
+wn.title("Pong by Gabe followed by @TokyoEdTech")
+wn.bgcolor("Black")
+wn.tracer(0)
+wn.setup(width=800, height=600)
+
+#score 
+
+score_a = 0
+score_b = 0
+#PAddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("Blue")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("Red")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("White")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("White")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+
+
+
+# functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard Binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+#main game loop
+
+while True:
+    wn.update()
+
+
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+
+    # Border Checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("afplay sfx-boing5.mp3&")
+
+
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy = 2
+        os.system("afplay sfx-boing5.mp3&")
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx = -2
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        os.system("afplay point_score.wav&")
+
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx = 2
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        os.system("afplay point_score.wav&")
+    
+    # Paddle and Ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350)  and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1 * (1 + 1 / 2)
+        os.system("afplay sfx-boing5.mp3&")
+
+    if (ball.xcor() < -340 and ball.xcor() > -350)  and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1 * (1 + 1 / 2)
+        os.system("afplay sfx-boing5.mp3&")
+
+    
